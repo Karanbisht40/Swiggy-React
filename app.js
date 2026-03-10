0c9a75d58bf87ef8f065998f16aa72058921edb3
Karan Bisht <karanbisht4041@gmail.com>
2026-03-10 22:35:35 +0530

    new project

---
+++ app.js
@@ -0,0 +1,12 @@
+import react from "react";
+import ReactDOM from "react-dom/client";
+
+
+function App(){
+
+    return(
+        <h1>hello this is web </h1>
+    )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App></App>);
