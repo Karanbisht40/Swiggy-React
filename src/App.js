9686e5db3b73a7da7b724943bd953f5e95905998
Karan Bisht <karanbisht4041@gmail.com>
2026-03-22 16:48:10 +0530

    sr

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client"
 import Header from "./Components/Header";
 import FoodOption from "./Components/FoodOption";
 import GroceryOption from "./Components/GroceryOption";
+import DineOption from "./Components/DineOption";
 
 
 
@@ -13,6 +14,7 @@ function App(){
       <Header></Header>
      <FoodOption></FoodOption>
     <GroceryOption></GroceryOption>
+    <DineOption></DineOption>
      </>
     )
 }
--- src/Components/GroceryOption.js
+++ src/Components/GroceryOption.js
@@ -3,9 +3,9 @@ import Grocerycard from "./GroceryCard";
 
 export default function(){
     return(
-          <div className="mt-20 w-[80%] container mx-auto ">
-          <h1> Shop Groceries on instamart</h1>
-          <div className=" w-[80%] container mx-auto flex flex-wrap mt-20 gap-3">
+          <div className="mt-20 w-[80%] container mx-auto mt-20 ">
+          <h1 className="text-3xl font-bold"> Shop Groceries on Instamart</h1>
+          <div className="  container mx-auto flex flex-nowrap  overflow-x-auto  gap-3">
        {
           GrocerGridCard.map((foodData)=><Grocerycard key={ foodData.id} foodData={foodData} ></Grocerycard>)
        }
