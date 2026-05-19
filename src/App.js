228a4c93288395f32431e368331298fc5c7f785b
Karan Bisht <karanbisht4041@gmail.com>
2026-05-19 17:49:08 +0530

    swiggy

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home></Home>}></Route>
                     <Route path="/restaurant" element={<Restaurant></Restaurant>}></Route>
-                    
                     <Route path="/city/delhi/:id" element={<RestaurantMenu></RestaurantMenu>}></Route>
 
                 </Routes>
